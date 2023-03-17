fd890555a16fc84d82b37d7acc35b860b1b40bd0
Martijnvdsteen <martijnvdsteen@florisoft.nl>
2023-03-17 14:19:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ViewComponentsApp
+ Dit is de ViewComponentsApp voor het afstudeeeronderzoek van Martijn van der Steen
---
+++ ViewComponents/SocialLinksViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsApp.Models;
+
+namespace ViewComponentsApp.ViewComponents
+{
+    public class SocialLinksViewComponent : ViewComponent
+    {
+        List<SocialIcon> socialIcons = new List<SocialIcon>();
+        public SocialLinksViewComponent()
+        {
+            socialIcons = SocialIcon.AppSocialIcons();
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = socialIcons;
+            return await Task.FromResult((IViewComponentResult)View("SocialLinks", model));
+        }
+    }
+}
